503783c0c79a693e1eb25df6f8bbba016b07d219
Matheus Goncalves <matheusgoncalves@air-de-matheus.lan>
2020-01-22 12:05:46 -0300

    create plans model

---
+++ src/app/models/Plans.js
@@ -0,0 +1,28 @@
+import Sequelize, { Model } from 'sequelize';
+
+class Plans extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        title: Sequelize.STRING,
+        duration: Sequelize.INTEGER,
+        price: Sequelize.DOUBLE
+      },
+      {
+        sequelize
+      }
+    );
+
+    this.addHook('beforeSave', async plans => {
+      plans.weight = parseFloat(await plans.duration.toFixed(2));
+      plans.height = parseFloat(await plans.price.toFixed(3));
+    });
+
+    this.addHook('beforeUpdate', async plans => {
+      plans.weight = parseFloat(await plans.duration.toFixed(2));
+      plans.height = parseFloat(await plans.price.toFixed(2));
+    });
+  }
+}
+
+export default Plans;
